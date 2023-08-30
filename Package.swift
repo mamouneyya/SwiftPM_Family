9ecd0f0f1aad5e176857e9a55f6b688e2f7aaeca
Ma'moun Diraneyya <diraneyy@audible.com>
2023-08-30 10:37:06 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -7,9 +7,15 @@ let package = Package(
     products: [
         .library(name: "Family", targets: ["Family"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "git@github.com:mamouneyya/SwiftPM_Daddy.git", from: "0.0.0"),
+        .package(url: "git@github.com:mamouneyya/SwiftPM_Mommy.git", from: "0.0.0"),
+    ],
     targets: [
-        .target(name: "Family", dependencies: []),
+        .target(name: "Family", dependencies: [
+            .product(name: "Daddy", package: "SwiftPM_Daddy"),
+            .product(name: "Mommy", package: "SwiftPM_Mommy"),
+        ]),
         .testTarget(name: "FamilyTests", dependencies: ["Family"]),
     ]
 )
