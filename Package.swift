325a9b487e782c4d76c57d60e9e10d60f85a62a1
Ma'moun Diraneyya <diraneyy@audible.com>
2023-08-30 10:44:45 -0400

    Expose specific products for partial graphs

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 let package = Package(
     name: "Family",
     products: [
-        .library(name: "Family", targets: ["Family"]),
+        .library(name: "WholeFamily", targets: ["FamilyTarget"]),
+        .library(name: "OnlyDaddyTree", targets: ["DaddyTarget"]),
+        .library(name: "OnlyMommyTree", targets: ["MommyTarget"]),
     ],
     dependencies: [
         .package(url: "git@github.com:mamouneyya/SwiftPM_Daddy.git", from: "0.0.0"),
         .package(url: "git@github.com:mamouneyya/SwiftPM_Mommy.git", from: "0.0.0"),
     ],
     targets: [
-        .target(name: "Family", dependencies: [
+        .target(name: "FamilyTarget", dependencies: [
             .product(name: "Daddy", package: "SwiftPM_Daddy"),
             .product(name: "Mommy", package: "SwiftPM_Mommy"),
         ]),
-        .testTarget(name: "FamilyTests", dependencies: ["Family"]),
+        .target(name: "DaddyTarget", dependencies: [
+            .product(name: "Daddy", package: "SwiftPM_Daddy"),
+        ]),
+        .target(name: "MommyTarget", dependencies: [
+            .product(name: "Mommy", package: "SwiftPM_Mommy"),
+        ]),
+        .testTarget(name: "FamilyTests", dependencies: ["FamilyTarget"]),
     ]
 )
